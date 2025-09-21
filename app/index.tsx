69745b9af5e717afbb3e3b80b43b0589832f13e7
Khang Tran <khangminh1472001@gmail.com>
2025-09-21 12:32:00 -0700

    added text resize inside widget

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,7 @@ import {
 } from "@gorhom/bottom-sheet";
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
-import { AutoSizeText, ResizeTextMode } from "react-native-auto-size-text";
+import { Dimensions, Text, TouchableOpacity, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import CustomizeSheet from "../components/CustomizeSheet";
 import WidgetPreview from "../components/WidgetPreview";
@@ -16,6 +15,23 @@ export default function Index() {
   const bottomSheetRef = useRef<BottomSheetModal>(null);
   const { theme, setTheme } = useTheme();
   const [horoscope, setHoroscope] = useState("Loading...");
+  const [widgetSize, setWidgetSize] = useState<"small" | "medium" | "large">(
+    "small"
+  );
+  const [currentFont, setCurrentFont] = useState(20)
+  useEffect(() => {
+    switch (widgetSize) {
+      case "small":
+        setCurrentFont(20);
+        break;
+      case "medium":
+        setCurrentFont(25);
+        break;
+      case "large":
+        setCurrentFont(30);
+        break;
+    }
+  }, [widgetSize]);
 
   function stripDate(raw: string) {
     if (!raw) return raw;
@@ -41,9 +57,9 @@ export default function Index() {
       };
       try {
         const response = await axios.request(options);
-        if (__DEV__) {
-          console.log(response.data.horoscope);
-        }
+        // if (__DEV__) {
+        //   console.log(response.data.horoscope);
+        // }
         const raw = response.data.horoscope || "";
         const horoscopeOnly = stripDate(raw);
         setHoroscope(horoscopeOnly || "No data found");
@@ -62,6 +78,28 @@ export default function Index() {
       <BottomSheetModalProvider>
         <View className="flex-1">
           {/* header */}
+          <View className="flex-row justify-around p-5">
+            <TouchableOpacity
+              className="border rounded-xl p-3"
+              onPress={() => setWidgetSize("small")}
+            >
+              <Text>Small</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              className="border rounded-xl p-3"
+              onPress={() => setWidgetSize("medium")}
+            >
+              <Text>Medium</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              className="border rounded-xl p-3"
+              onPress={() => setWidgetSize("large")}
+            >
+              <Text>Large</Text>
+            </TouchableOpacity>
+          </View>
           <View className="items-center justify-center p-10">
             <Text className="text-3xl font-bold">Daily Horoscope</Text>
             <Text className="text-2xl mt-3">Zodiac</Text>
@@ -71,27 +109,57 @@ export default function Index() {
             {/* widget */}
             {/* using my-auto will take up the whole vertical space */}
             <WidgetPreview
-              size="medium"
+              size={widgetSize}
               style={{
                 backgroundColor: theme.bgColor,
                 alignSelf: "center",
                 margin: "auto",
               }}
             >
-                <AutoSizeText
-                  fontSize={20}
-                  numberOfLines={1000}
-                  mode={ResizeTextMode.max_lines}
-                  style={{
-                    color: theme.textColor,
-                    fontFamily: theme.textFont,
-                    textAlign: "center",
-                    //includeFontPadding: false,
-                    textAlignVertical: "center",
-                  }}
-                >
-                  {horoscope}
-                </AutoSizeText>
+              <Text
+                style={{
+                  fontSize: currentFont,
+                  color: theme.textColor,
+                  fontFamily: theme.textFont,
+                  textAlign: "center",
+                  textAlignVertical: "center",
+                }}
+                onTextLayout={(e) => {
+                  const { lines } = e.nativeEvent;
+
+                  // calculate total text height
+                  const textHeight = lines.reduce(
+                    (acc, line) => acc + line.height,
+                    0
+                  );
+
+                  const iosSizes: Record<
+                    "small" | "medium" | "large",
+                    { w: number; h: number }
+                  > = {
+                    small: { w: 155, h: 155 },
+                    medium: { w: 329, h: 155 },
+                    large: { w: 329, h: 345 },
+                  };
+                  const scale = Dimensions.get("window").width / 375;
+                  const containerHeight = iosSizes[widgetSize].h * scale - 20;
+
+                  console.log("*********** tH: " + textHeight)
+                  console.log("*********** cH: " + containerHeight)
+
+                  if (textHeight > containerHeight) {
+                    setCurrentFont(currentFont - 0.5); // shrink gradually
+                    console.log("Font decreased to: " + (currentFont - 0.5));
+                  }
+                }}
+              >
+                You're ready to soar like a bird, Cancer. When the next gust of
+                wind comes up, you'll take flight and send your beautiful
+                presence all over the world. Try not to spread yourself too
+                thin, but feel free to soar to every corner of the field.
+                There's an expansive feeling in the air today that gives a boost
+                to your emotions.
+              </Text>
             </WidgetPreview>
 
             {/* customize button */}
