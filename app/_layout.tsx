82b99d970d7ebcb2b0a2d3e61753454f9ae6ce30
Khang Tran <khangminh1472001@gmail.com>
2025-10-01 14:36:06 -0700

    moving on to adding widget

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ActivityIndicator, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FontProvider, useFont } from "../contexts/FontContext";
-import { HoroscopeProvider, useHoroscope } from "../contexts/HoroscopeContext";
+import { HoroscopeProvider } from "../contexts/HoroscopeContext";
 import { ThemeProvider, useTheme } from "../contexts/ThemeContext";
 import "./globals.css";
 
@@ -22,7 +22,6 @@ export default function RootLayout() {
 function Content() {
   const { fontsLoaded } = useFont();
   const { theme } = useTheme();
-  const { horoscope } = useHoroscope();
 
   if (!fontsLoaded) {
     return (
