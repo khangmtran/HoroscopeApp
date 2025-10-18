6fa67526a628308dc1dbf0200e59833574d2afca
Khang Tran <khangminh1472001@gmail.com>
2025-10-17 22:27:44 -0700

    horoscope service throught a backend

--- services/HoroscopeService.tsx
+++ services/HoroscopeService.tsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 class HoroscopeService {
+  private static BACKEND_URL = "https://horoscope-backend-dwnh.onrender.com";
+
   private static stripDate(raw: string): string {
     if (!raw) return raw;
 
-    // Find the index of the first '-'
     const dashIndex = raw.indexOf("-");
     if (dashIndex !== -1) {
       return raw.substring(dashIndex + 1).trim();
@@ -16,30 +17,13 @@ class HoroscopeService {
     dob: string = "2025-01-20",
     topic: string = "general"
   ): Promise<string> {
-    const normalizedTopic = topic.toLowerCase();
-    const url =
-      normalizedTopic === "general"
-        ? "https://the-numerology-api.p.rapidapi.com/horoscope/today"
-        : `https://the-numerology-api.p.rapidapi.com/horoscope/${normalizedTopic}/today`;
-    const apiKey = process.env.EXPO_PUBLIC_RAPIDAPI_KEY;
-    if (!apiKey) {
-      throw new Error(
-        "API key not found. Set EXPO_PUBLIC_RAPIDAPI_KEY in .env file"
-      );
-    }
-    const options = {
-      method: "GET",
-      url,
-      params: { dob },
-      headers: {
-        "x-rapidapi-key": apiKey,
-        "x-rapidapi-host": "the-numerology-api.p.rapidapi.com",
-      },
-    };
-
     try {
-      const response = await axios.request(options);
-      const raw = response.data.horoscope || "";
+      const response = await axios.post(`${this.BACKEND_URL}/api/horoscope`, {
+        dob,
+        topic,
+      });
+
+      const raw = response.data.data.horoscope || "";
       const horoscopeOnly = this.stripDate(raw);
       return horoscopeOnly || "No data found";
     } catch (error) {
